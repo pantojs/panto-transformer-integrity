9dec50923fb89f76d68c72ce688be5d62097da5c
yinyong <yanni4night@gmail.com>
2016-06-22 13:58:05 +0800

    ready

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # panto-transformer-integrity
-Integrity transformer for panto
+[![NPM version][npm-image]][npm-url] [![Downloads][downloads-image]][npm-url] [![Dependency status][david-dm-image]][david-dm-url] [![Dev Dependency status][david-dm-dev-image]][david-dm-dev-url]
+
+[Integrity](https://w3c.github.io/webappsec-subresource-integrity/) transformer for panto.
+
+```js
+panto.loadTransformer('integrity');
+
+panto.pick('**/*.{css,js}').pipe(panto.read()).pipe(panto.integrity()).end();
+```
+
+[npm-url]: https://npmjs.org/package/panto-transformer-integrity
+[downloads-image]: http://img.shields.io/npm/dm/panto-transformer-integrity.svg
+[npm-image]: http://img.shields.io/npm/v/panto-transformer-integrity.svg
+[david-dm-url]:https://david-dm.org/pantojs/panto-transformer-integrity
+[david-dm-image]:https://david-dm.org/pantojs/panto-transformer-integrity.svg
+[david-dm-dev-url]:https://david-dm.org/pantojs/panto-transformer-integrity#info=devDependencies
+[david-dm-dev-image]:https://david-dm.org/pantojs/panto-transformer-integrity/dev-status.svg
---
+++ index.js
@@ -0,0 +1,66 @@
+/**
+ * Copyright (C) 2016 pantojs.xyz
+ * index.js
+ *
+ * changelog
+ * 2016-06-22[13:12:07]:revised
+ *
+ * @author yanni4night@gmail.com
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+'use strict';
+const Transformer = require('panto-transformer');
+const crypto = require('crypto');
+
+class IntegrityTransformer extends Transformer {
+    _transform(file) {
+        const {
+            filename,
+            content
+        } = file;
+        const algorithms = ['sha256', 'sha384', 'sha512'];
+        const {
+            algorithm
+        } = this.options;
+
+        const tryOne = algorithm => {
+            try {
+                let sum = algorithm + '-' + crypto.createHash(algorithm).update(content).digest().toString(
+                    'base64');
+                return sum;
+            } catch (e) {
+                panto.log.warn(`IntegrityTransform error in ${filename}: ${e.messsage}`);
+            }
+        };
+
+        const tryN = () => {
+            for (let i = 0; i < algorithms.length; ++i) {
+                let sum = tryOne(algorithms[i]);
+                if (sum) {
+                    return sum;
+                }
+            }
+        };
+
+        let sum;
+
+        if (panto.util.isUndefined(algorithm)) {
+            sum = tryN();
+        } else if (algorithms.indexOf(algorithm) > -1) {
+            sum = tryOne(algorithm);
+        } else {
+            panto.log.error(`algorithm must be one of ${algorithms.join()}`);
+        }
+
+        if (sum) {
+            return Promise.resolve(file);
+        }
+
+        return Promise.resolve(panto.util.extend(file, {
+            integrity: sum
+        }));
+    }
+}
+
+module.exports = IntegrityTransformer;
